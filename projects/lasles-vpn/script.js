7042431c3939f37b1c93f49f8624c9a03c4a0633
Sokrat <lumen437@gmail.com>
2020-11-19 15:25:04 +0400

    added lasles project version 1.0

---
+++ projects/lasles-vpn/script.js
@@ -0,0 +1,57 @@
+// header shrink with scroll
+
+window.onscroll = function () {
+    if (document.documentElement.scrollTop > 120) {
+        document.querySelector(".header").style.padding = "10px 20px";
+    } else if (document.documentElement.scrollTop == 0) {
+        document.querySelector(".header").style.padding = "20px 20px";
+    }
+}
+
+//slider part
+
+let prev = document.querySelector('.prev'),
+    next = document.querySelector('.next'),
+    list = document.querySelector('.carousel-list'),
+    items = document.querySelectorAll('.carousel-item'),
+    itemsCount = items.length,
+    slidesToScroll = 2,
+    slidesToShow = 3,
+    itemWidth = 450,
+    movePosition = slidesToScroll * itemWidth,
+    position = 0
+
+prev.addEventListener('click', () => {
+    position += movePosition;
+    setPosition();
+    checkBtns();
+})
+
+next.addEventListener('click', () => {
+    position -= movePosition;
+    setPosition();
+    checkBtns();
+})
+
+const setPosition = () => {
+    list.style.transform = `translateX(${position}px)`;
+}
+const checkBtns = () => {
+    prev.disabled = position === 0;
+    next.disabled = position <= -(itemsCount - slidesToShow) * (itemWidth + itemWidth / 2);
+};
+checkBtns();
+
+//map part
+
+let mapPoints = document.querySelectorAll('.map-point');
+
+setInterval(() => {
+    let transformedPoint = mapPoints[Math.floor(Math.random() * mapPoints.length)];
+
+    transformedPoint.style.transform = "scale(2, 2)";
+    setTimeout(() => {
+        transformedPoint.style.transform = "scale(1, 1)";
+    }, 500)
+
+}, 2500);
